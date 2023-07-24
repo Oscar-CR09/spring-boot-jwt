b27c430a9612ab4f05ec0db851e2f0508040ab97
Oscar <oscarcr189@outlook.com>
2023-07-23 19:30:18 -0600

    JWTAuthentication agregando

--- src/main/java/com/examples/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/examples/springboot/app/SpringSecurityConfig.java
@@ -3,7 +3,6 @@ package com.examples.springboot.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import com.examples.springboot.app.auth.filter.JWTAuthenticationFilter;
-import com.examples.springboot.app.auth.filter.JWTAuthorizationFilter;
 import com.examples.springboot.app.models.service.JpaUserDetailsService;
 
 
@@ -43,8 +40,6 @@ public class SpringSecurityConfig {
 					//.logout((logout) -> logout.logoutUrl("/","/login"))
 			
 					//		.permitAll())
-					.addFilter(new JWTAuthenticationFilter(authenticationManager()))
-					.addFilter(new JWTAuthorizationFilter(authenticationManager()))
 					.csrf(csrf -> csrf.disable())
 					.sessionManagement((session) -> session
 				    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -67,9 +62,4 @@ public class SpringSecurityConfig {
 		//.usersByUsernameQuery("select usuario, passoword, enabled from usuarios where usuario=?")
 		//.authoritiesByUsernameQuery("select u.usuario, a.authority from authorities a inner join usuarios u on (a.user_id=u.id) where u.usuario=?");
 	}
-	public AuthenticationManager authenticationManager() {
-		
-		return null;
-	}
-
 }
--- src/main/java/com/examples/springboot/app/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/examples/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.examples.springboot.app.auth.SimpleGrantedAuthorityMixin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
@@ -22,6 +23,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
 
 	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
 		super(authenticationManager);
@@ -55,7 +57,9 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 			String username = token.getSubject();
 			Object roles = token.get("authorities");
 			
-			Collection<? extends GrantedAuthority> authorities = Arrays.asList(new ObjectMapper().readValue(roles.toString(), SimpleGrantedAuthority[].class));
+			Collection<? extends GrantedAuthority> authorities = Arrays.asList(new ObjectMapper()
+					.addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityMixin.class)
+					.readValue(roles.toString(), SimpleGrantedAuthority[].class));
 					
 			authentication = new UsernamePasswordAuthenticationToken(username, null,authorities);
 		}
